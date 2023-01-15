a379722aa9c4bc6bcb2382b7fc68ceb7cd425194
Yen Xiong Yuan <yenyuan17@gmail.com>
2023-01-15 00:53:43 +0000

    finish challenge 1

--- javascript/code-challenges/challenges-01.test.js
+++ javascript/code-challenges/challenges-01.test.js
@@ -58,47 +58,65 @@ CHALLENGE 4
 
 Write a function named `greeting` that takes in a single string and returns the string in all uppercase letters, and followed by an "!".
 
-Then, write a function named `speaker` that takes in an array of strings and a callback function.
+Then, write a function named `speaker` that takes in an array of strings and a callback function. ******
 
 Use `forEach` to build a new array of strings, each string modified by the callback. Return the new array.
+
+test('It should provide an array of strings, that get uppercased, and a "!" at the end', () => {
+    expect(speaker(["hello", "301", "students"], greeting)).toStrictEqual([
+      "HELLO!",
+      "301!",
+      "STUDENTS!",
+    ]);
 ------------------------------------------------------------------------------------------------ */
 
 const greeting = (word) => {
   // Solution code here...
-  const upperCase = [];
-  word.forEach((string) => {
-    upperCase.push(`${string.toUpperCase()}!`);
-  });
-  return upperCase;
+  return `${word.toUpperCase()}!`;
 };
 
 const speaker = (words, callback) => {
   // Solution code here...
-  return speaker(greeting());
+  const newWords = [];
+  words.forEach((word) => {
+    newWords.push(callback(word));
+  });
+  return newWords;
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
 
-Write a function named addValues that takes in an array and a value and pushes the value into the array. This function does not need a return statement.
+Write a function named addValues that takes in an array and a value and -----
+pushes the value into the array. This function does not need a return statement.
 
 Then, write a function named addNumbers that takes in four arguments:
-  - A number to be added to an array
-  - An array into which the number should be added
+  - A number to be added to an array   num ->> [num]
   - The number of times the number should be added
+  - An array into which the number should be added
   - A callback function to use to add the numbers to the array (Hint: you already defined it)
 
 Within the addNumbers function, invoke the callback function as many times as necessary, based on the third argument of the addNumbers function.
 
 Return the modified array.
+
+test("It should add the number 8 to the array five times", () => {
+    expect(addNumbers(8, [], 5, addValues)).toStrictEqual([8, 8, 8, 8, 8]);
+    expect(addNumbers(8, [], 5, addValues).length).toStrictEqual(5);
+  });
 ------------------------------------------------------------------------------------------------ */
 
 const addValues = (arr, value) => {
   // Solution code here...
+  arr.push(value);
 };
 
 const addNumbers = (num, arr, times, callback) => {
   // Solution code here...
+  for (let i = 0; i < times; i++) {
+    callback(arr, num);
+  }
+  return arr;
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -117,10 +135,22 @@ The inventory is formatted like this:
 ]
 
 This function should use forEach to populate your grocery list based on the store's inventory. If the item is available, add it to your list. Return the final list.
+
+expect(createList(inventory)).toStrictEqual(["apples", "pears", "bananas"]);
+  expect(createList(inventory).length).toStrictEqual(3);
+
 ------------------------------------------------------------------------------------------------ */
 
 const createList = (availableItems) => {
   // Solution code here...
+  const avaItem = [];
+
+  availableItems.forEach((itemobj) => {
+    if (itemobj.available === true) {
+      avaItem.push(itemobj.name);
+    }
+  });
+  return avaItem;
 };
 
 /* ------------------------------------------------------------------------------------------------
