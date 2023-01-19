26bccd2f024a7aabf7b531d1f79070690e4d314c
Yen Xiong Yuan <yenyuan17@gmail.com>
2023-01-19 04:56:27 +0000

    work on challenge 8

--- javascript/code-challenges/challenges-03.test.js
+++ javascript/code-challenges/challenges-03.test.js
@@ -78,7 +78,8 @@ const oddValues = (arr) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 5
 
-Write a function named notInFirstArray that, given two arrays as input, uses filter to return an array of all the elements in the second array that are not included in the first array.
+1. Write a function named notInFirstArray that, given two arrays as input,
+2. uses filter to return an array of all the elements in the second array that are not included in the first array.
 
 For example, notInFirstArray([1,2,3], [1,2,3,4]) returns [4].
 ------------------------------------------------------------------------------------------------ */
