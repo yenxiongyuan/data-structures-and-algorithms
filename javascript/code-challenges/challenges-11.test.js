3e40d47a8a465295dcf2ab4d5e4c2a087412e642
Yen Xiong Yuan <yenyuan17@gmail.com>
2023-01-28 21:03:31 +0000

    finish challenge 11

--- javascript/code-challenges/challenges-11.test.js
+++ javascript/code-challenges/challenges-11.test.js
@@ -3,7 +3,8 @@
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 1 - Review
 
-Write a function named transformToLis that, given an object, returns an array of the key value pairs as html list items.
+1. Write a function named transformToLis that, given an object,
+2. returns an array of the key value pairs as html list items.
 
 For example:
 {
@@ -20,6 +21,9 @@ Becomes:
 
 function transformToLis(obj){
   // Solution code here...
+  return Object.entries(obj).map(
+    (property) => `<li>${property[0]}: ${property[1]}</li>`
+  );
 }
 
 /* ------------------------------------------------------------------------------------------------
@@ -44,7 +48,8 @@ const count = (target, input) => {
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 3
 
-Write a function that, given an array of integer arrays as input, calculates the total sum of all the elements in the array.
+1. Write a function that, given an array of integer arrays as input,
+2. calculates the total sum of all the elements in the array.
 
 You may want to use filter, map, or reduce for this problem, but are not required to. You may need to use the same method more than once.
 
@@ -53,22 +58,30 @@ For example, [[1, 2, 3, 4, 5], [6, 7, 2, 4, 5, 7], [9, 2, 3, 6,]] returns 66.
 
 const totalSum = (input) => {
   // Solution code here...
+  return input
+    .flat()
+    .reduce((runningTotal, currentValue) => runningTotal + currentValue, 0);
 };
 
 /* ------------------------------------------------------------------------------------------------
 CHALLENGE 4
 
-Write a function named divisibleByFiveTwoToThePower that accepts an array of arrays as input.
+1. Write a function named divisibleByFiveTwoToThePower that accepts an array of arrays as input.
 
-This function should first remove any elements that are not numbers or are not divisible by five.
+2. This function should first remove any elements that are not numbers or are not divisible by five.
 
-This function should then raise 2 to the power of the resulting numbers, returning an array of arrays.
+3. This function should then raise 2 to the power of the resulting numbers, returning an array of arrays.
 
 For example, [ [0,2,5,4], [2,4,10], [] ] should return [ [1, 32], [1024], [] ].
 ------------------------------------------------------------------------------------------------ */
 
 const divisibleByFiveTwoToThePower = (input) => {
   // Solution code here...
+  return input.map((nestedArr) => {
+    return nestedArr
+      .filter((number) => number % 5 === 0 && typeof number === "number")
+      .map((filteredNumber) => Math.pow(2, filteredNumber));
+  });
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -135,6 +148,9 @@ let starWarsData = [{
 
 let findMaleAndFemale = (data) => {
   // Solution code here...
+  return data.filter(obj => obj.gender.includes('male'))
+    .map(person => person.name)
+    .join(' and ');
 };
 
 /* ------------------------------------------------------------------------------------------------
@@ -145,6 +161,9 @@ Write a function named findShortest that, given the Star Wars data from Challeng
 
 let findShortest = (data) => {
   // Solution code here...
+  return data.reduce((shortestSoFar, nextPerson) =>
+    +shortestSoFar.height < +nextPerson.height ? shortestSoFar : nextPerson
+  ).name;
 };
 
 /* ------------------------------------------------------------------------------------------------
