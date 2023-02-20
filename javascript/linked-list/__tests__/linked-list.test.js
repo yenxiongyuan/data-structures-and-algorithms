57f3db86fb0ed356e9a889b9abf0a4107328670f
Yen Xiong Yuan <yenyuan17@gmail.com>
2023-02-20 01:53:52 +0000

    add link-list

--- javascript/linked-list/__tests__/linked-list.test.js
+++ javascript/linked-list/__tests__/linked-list.test.js
@@ -1,10 +1,59 @@
-'use strict';
+"use strict";
 
 // Require our linked list implementation
-const LinkedList = require('../index');
+const LinkedList = require("../index");
 
-describe('Linked List', () => {
-  it('works', () => {
-    expect(true).toBeTruthy();
+//Create an empty list
+describe("Linked List", () => {
+  it("should instantiate an empty linked list", () => {
+    const linked = new LinkedList();
+
+    expect(linked.head).toBeNull(); // head (Node) is undefined, set the head to null in constructor index.js
+  });
+
+  // Insert #1
+  it("should insert at beginning of empty list", () => {
+    const linked = new LinkedList();
+    linked.insert("alligator"); // add a value (alligator) to the linked list have Node, make class Node in index.js
+
+    expect(linked.head.value).toEqual("alligator"); // head is Node, linked.head(Node).value
+    expect(linked.head.next).toBeNull();
+  });
+
+  it("should insert at beginning of populated list", () => {
+    const linked = new LinkedList();
+    linked.insert("alligator"); // first insert alligator current head
+    linked.insert("bear"); // then insert bear, then become the current head push alligator over
+
+    expect(linked.head.value).toEqual("bear"); //current head
+    expect(linked.head.next.value).toEqual("alligator"); //Insert #4 No Link this head to old head, head is undefined. Make new head add to old head in index.js at (insert) want to keep alligator in process
+    expect(linked.head.next.next).toBeNull(); // 3 value will be head.next.next.next
+  });
+
+  // To string #1
+  it("should display as string properly", () => {
+    const linked = new LinkedList();
+    linked.insert("alligator");
+    linked.insert("bear");
+
+    const linkedString = linked.toString(); //linked.toString() return object '[object, object]', need to make a toString() in index.js
+
+    expect(linkedString).toEqual("{ bear } -> { alligator } -> NULL");
+  });
+
+  it("should know when value is included in list", () => {
+    const linked = new LinkedList();
+    linked.insert("alligator");
+    linked.insert("bear");
+
+    expect(linked.includes("alligator")).toBe(true);
+  });
+
+  it("should know when value is not included in list", () => {
+    const linked = new LinkedList();
+    linked.insert("alligator");
+    linked.insert("bear");
+
+    expect(linked.includes("cat")).toBe(false);
   });
 });
--- javascript/linked-list/index.js
+++ javascript/linked-list/index.js
@@ -1,7 +1,59 @@
-'use strict';
+"use strict";
 
+//class is an object
 class LinkedList {
+  constructor() {
+    this.head = null;
+  }
 
+  // Insert #3
+  insert(value) {
+    //Insert #4
+    const oldHead = this.head; //Insert #4
+    this.head = new Node(value); // Insert #3, this = LindedList
+    this.head.next = oldHead; //Insert #4
+
+    // or
+    // this.head = new Node(value, this.head); Insert #4 new Node(value, this.head = next, this.head before reassigned)
+  }
+
+  // To string #2 overwrite the toString to parent
+  toString() {
+    let current = this.head; //use let will change
+    let text = "";
+
+    // walk around
+    while (current) {
+      //current is = null(falsy) or current is = Node(truthy)
+      // do something
+      text += `{ ${current.value} } -> `; // `{ ${current.value} } -> ` = '{ bear } -> { alligator } ->' and text + "NULL" for the return
+
+      current = current.next; // move to next = current.next, move to last one is null and out of while loop
+    }
+    // return '{ bear } -> { alligator } -> NULL';
+
+    return text + "NULL";
+  }
+
+  includes(value) {
+    let current = this.head;
+    while (current !== null) {
+      if (current.value === value) {
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
+}
+
+  // Insert #2
+class Node {
+                      //null= default value for next
+  constructor(value, next = null) {
+    this.value = value;
+    this.next = next;
+  }
 }
 
 module.exports = LinkedList;
